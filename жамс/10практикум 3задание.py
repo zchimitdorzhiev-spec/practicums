0c606de46b6bf1f2f965b0d784e0d8d22f12debf
zchimitdorzhiev-spec <z.chimitdorzhiev@g.nsu.ru>
2026-05-23 14:33:08 +0700

    Add files via upload

---
+++ жамс/10практикум 3задание.py
@@ -0,0 +1,31 @@
+def sale(discontcard, public_holiday):
+    price = int(input('Введите сумму покупки: '))
+    sale_percent = 0
+
+    if discontcard.lower() == 'да':
+        sale_percent += 0.05
+
+    if public_holiday.lower() == 'да':
+        sale_percent += 0.03
+
+    if 5000 <= price < 15000:
+        sale_percent += 0.03
+    elif 15000 <= price < 20000:
+        sale_percent += 0.05
+    elif 20000 <= price < 30000:
+        sale_percent += 0.07
+    elif price >= 30000:
+        sale_percent += 0.1
+
+    if sale_percent > 0.15:
+        sale_percent = 0.15
+
+    total_price = price * (1 - sale_percent)
+
+    print(f'Сумма со скидкой: {total_price:.2f} руб.')
+    print(f'Скидка составила: {sale_percent * 100:.0f}%')
+
+    return total_price
+
+
+sale('Да', 'Да')
